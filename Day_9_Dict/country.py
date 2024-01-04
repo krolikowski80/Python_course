26c6ed635b5a7febfd2d90567d598900b9a05044
Tomasz Królikowski <t.krolikowski@gmail.com>
2024-01-04 18:59:51 +0100

    Dzień 9

---
+++ Day_9_Dict/country.py
@@ -0,0 +1,25 @@
+country = input("Add country name: ")  # Add country name
+visits = int(input("Number of visits: "))  # Number of visits
+list_of_cities = eval(input("Add cities: "))  # create list from formatted string
+
+travel_log = [
+    {"country": "France", "visits": 12, "cities": ["Paris", "Lille", "Dijon"]},
+    {"country": "Germany", "visits": 5, "cities": ["Berlin", "Hamburg", "Stuttgart"]},
+]
+# Do NOT change the code above
+
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log.
+def add_new_country(country, time_visits, cities_visited):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = time_visits
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
+
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
